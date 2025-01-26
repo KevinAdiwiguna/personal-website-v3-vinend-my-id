195cb945d85ef8d248c0eae947d872314f3ef905
KevinAdiwiguna <adiwigunakevin@gmail.com>
2025-01-26 23:33:28 +0800

    feat: update tech action

--- src/actions/technology-action.ts
+++ src/actions/technology-action.ts
@@ -1,20 +1,13 @@
-"use server"
+"use server";
 
-import { db } from "@/db/db"
-import { revalidatePath } from "next/cache"
-import { redirect } from "next/navigation"
+import { db } from "@/db/db";
+import { revalidatePath } from "next/cache";
 
-import cloudinary from "@/lib/cloudinary"
+import cloudinary from "@/lib/cloudinary";
+import { ITEMS_PER_PAGE, QueryParamsProps, ResponseState } from "@/types/globals";
 
-interface GetAllTechnologyProps {
-  query: string
-  page: number
-}
-
-const ITEMS_PER_PAGE = 15
-
-export const GetAllTechnology = async ({ query, page }: GetAllTechnologyProps) => {
-  const offest = (page - 1) * ITEMS_PER_PAGE
+export const GetAllTechnology = async ({ query, page }: QueryParamsProps) => {
+  const offest = (page - 1) * ITEMS_PER_PAGE;
 
   try {
     const users = await db.technology.findMany({
@@ -26,18 +19,26 @@ export const GetAllTechnology = async ({ query, page }: GetAllTechnologyProps) =
           {
             tech: {
               contains: query,
-              mode: "insensitive"
-            }
-          }
-        ]
-      }
-    })
-    return users
+              mode: "insensitive",
+            },
+          },
+        ],
+      },
+    });
+    return {
+      status: 200,
+      message: "Technology fetched successfully",
+      data: users,
+      timeStamp: new Date(),
+    };
   } catch (error) {
-    throw new Error(`Failed to fetch contact data ${error}`);
+    return {
+      stauts: 500,
+      message: `Failed to fetch technology ${error}`,
+      timeStamp: new Date(),
+    };
   }
-
-}
+};
 
 export const GetTechnologyByCount = async (query: string) => {
   const user = await db.technology.count({
@@ -47,43 +48,62 @@ export const GetTechnologyByCount = async (query: string) => {
         {
           tech: {
             contains: query,
-            mode: "insensitive"
-          }
-        }
-      ]
-    }
-  })
+            mode: "insensitive",
+          },
+        },
+      ],
+    },
+  });
   const totalPages = Math.ceil(Number(user) / ITEMS_PER_PAGE);
   return totalPages;
-}
+};
 
-export const DeleteUTechnologyByID = async (formData: FormData) => {
-  const id = formData.get("id") as string
+export const DeleteUTechnologyByID = async (previousState: unknown, formData: FormData): Promise<ResponseState> => {
+  const id = formData.get("id") as string;
 
-  if (!id) {
-    throw new Error("Invalid user id");
+  if (!id || isNaN(parseInt(id))) {
+    return {
+      status: 400,
+      message: "Invalid user id",
+      timeStamp: new Date(),
+    };
   }
 
   try {
     await db.technology.update({
       where: {
-        id: parseInt(id)
-      }, data: {
-        deletedAt: new Date()
-      }
-    })
-    revalidatePath("/dashboard/technology");
+        id: parseInt(id),
+      },
+      data: {
+        deletedAt: new Date(),
+      },
+    });
+    return {
+      status: 200,
+      message: "Technology deleted successfully",
+      timeStamp: new Date(),
+    };
   } catch (error) {
-    throw new Error(`Failed to delete user ${error}`);
+    return {
+      status: 500,
+      message: `Failed to delete technology ${error}`,
+      timeStamp: new Date(),
+    };
+  } finally {
+    revalidatePath("/dashboard/technology");
   }
-}
+};
 
-export const CreateTechnology = async (formData: FormData) => {
+export const CreateTechnology = async (previousState: unknown, formData: FormData): Promise<ResponseState> => {
   const image = formData.get("images") as File;
   const tech = formData.get("tech") as string;
 
   if (!image || !(image instanceof File)) {
-    throw new Error("Invalid or missing image file in formData.");
+    return {
+      status: 400,
+      message: "Invalid or missing image file in formData.",
+      timeStamp: new Date(),
+    }
   }
 
   const arrayBuffer = await image.arrayBuffer();
@@ -91,17 +111,19 @@ export const CreateTechnology = async (formData: FormData) => {
 
   try {
     const result = await new Promise((resolve, reject) => {
-      cloudinary.uploader.upload_stream(
-        { folder: "technology", allowed_formats: ["heic", "gif", "jpg", "png", "webp", "mp4"] },
-        (error, result) => {
-          if (error) {
-            reject(error);
-            return;
-          }
+      cloudinary.uploader
+        .upload_stream(
+          { folder: "technology", allowed_formats: ["heic", "gif", "jpg", "png", "webp", "mp4"] },
+          (error, result) => {
+            if (error) {
+              reject(error);
+              return;
+            }
 
-          resolve(result?.url);
-        }
-      ).end(buffer);
+            resolve(result?.url);
+          }
+        )
+        .end(buffer);
     });
 
     await db.technology.create({
@@ -111,11 +133,19 @@ export const CreateTechnology = async (formData: FormData) => {
       },
     });
 
-    revalidatePath("/dashboard/technology");
+    return {
+      status: 200,
+      message: "Technology created successfully",
+      timeStamp: new Date(),
+    };
+
   } catch (error) {
-    console.error("Error creating technology:", error);
-    throw new Error(`Failed to create technology: ${error}`);
+    return {
+      status: 500,
+      message: `Failed to create technology ${error}`,
+      timeStamp: new Date(),
+    };
   } finally {
-    redirect("/dashboard/technology");
+    revalidatePath("/dashboard/technology");
   }
 };
