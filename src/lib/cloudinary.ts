3b27927458089ee6ddc60b2e045d3e3598798742
KevinAdiwiguna <f1d02410115.student.unram.ac.id>
2025-06-06 12:46:38 +0800

    add .env.example

--- src/lib/cloudinary.ts
+++ src/lib/cloudinary.ts
@@ -1,18 +1,17 @@
-import { v2 as cloudinary } from 'cloudinary'
-
+import { v2 as cloudinary } from "cloudinary";
 
 const cloudname = process.env.CLOUDINARY_CLOUD_NAME!;
 const apikey = process.env.CLOUDINARY_API_KEY!;
 const apisecret = process.env.CLOUDINARY_API_KEY_SECRET!;
 
 if (!cloudname || !apikey || !apisecret) {
-	throw new Error("Cloudinary credentials are missing");
+  throw new Error("Cloudinary credentials are missing");
 }
 
 cloudinary.config({
   cloud_name: cloudname,
   api_key: apikey,
-  api_secret: apisecret
-})
+  api_secret: apisecret,
+});
 
-export default cloudinary
+export default cloudinary;
